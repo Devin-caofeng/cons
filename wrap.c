b3d71be8873b02a907d012e7d4875ea67e3b7ed0
devincaofeng <devin.caofeng@gmail.com>
2018-03-04 09:35:10 +0800

    correct wrap file

--- wrap.c
+++ wrap.c
@@ -130,6 +130,29 @@ ssize_t WriteN(int fd, const void *vptr, size_t n) {
     return n - nleft;
 }
 
+static ssize_t MyRead(int fd, char *ptr) {
+    static int read_cnt;
+    static char *read_ptr;
+    static char read_buf[100];
+
+    if (read_cnt <= 0) {
+again:
+        if ((read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
+            if (errno == EINTR) goto again;
+            else return -1;
+        }
+        else if (read_cnt == 0) {
+            return 0;
+        }
+
+        read_ptr = read_buf;
+    }
+    --read_cnt;
+    *ptr = *read_ptr++;
+
+    return 1;
+}
+
 size_t ReadLine(int fd, void *vptr, ssize_t max_len) {
     ssize_t n, rc;
     char c, *ptr;
